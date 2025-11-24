b8f9ae1b6f6896e6fc4f4ef42e04ef300e258a5a
Richard6141 <somasse-richard.salanon@epitech.eu>
2025-11-24 17:22:08 +0100

    feat: ajout de la fonctionnalité d'inscription

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) { }
+
+    async register(dto: AuthDto) {
+        //Générer le mot de passe haché
+        const hash = await argon.hash(dto.password);
+        //Sauvegarder l'utilisateur dans la base de données avec la gestion des erreurs
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    passwordHash: hash,
+                },
+            });
+            const { passwordHash: _, ...userWithoutpasswordHash } = user;
+            return userWithoutpasswordHash;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Email already exists');
+                }
+            }
+            throw error;
+        }
+    }
 
     login() {
         return 'Hola Mundo';
-    }
-
-    async register(dto: AuthDto) {
-        const hash = await argon.hash(dto.password);
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                password: hash,
-            },
-        });
-        const { password: _, ...userWithoutPassword } = user;
-        return userWithoutPassword;
     }
 
     logout() {
